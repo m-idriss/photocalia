8e62142bef42c12d7b447b5f8e92560c956a4e35
Idriss <44480885+m-idriss@users.noreply.github.com>
2026-03-19 00:25:25 +0100

    :lipstick: style: match How It Works CTA design with Blog

--- src/app/pages/how-it-works/how-it-works.ts
+++ src/app/pages/how-it-works/how-it-works.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { TranslatePipe } from '../../shared/pipes/translate.pipe';
+import { LocalizeRoutePipe } from '../../shared/pipes/localize-route.pipe';
 
 @Component({
   selector: 'app-how-it-works',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, RouterModule, LocalizeRoutePipe],
   templateUrl: './how-it-works.html',
   styleUrl: './how-it-works.scss',
 })
